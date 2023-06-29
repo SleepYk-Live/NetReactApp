08d83ad08e72988bdddbe1bb637420e8dec508c5
Семен Попов <s.popov@avto.pro>
2023-06-29 10:38:51 +0300

    Add mobX

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,16 +1,12 @@
 import React, { SyntheticEvent, useState } from 'react'
-import { Activity } from '../../../app/models/activity'
 import { Button, Item, Label, Segment } from 'semantic-ui-react';
+import { useStore } from '../../../app/stores/store';
+import { observer } from 'mobx-react-lite';
 
-interface Props{
-    activities: Activity[];
-    submitting: boolean,
-    selectActivity: (id: string) => void;
-    deleteActivity: (id:string) => void;
-}
-
-export default function ActivityList({activities, selectActivity, deleteActivity, submitting}: Props){
+export default observer(function ActivityList(){
     const [target, setTarget] = useState('');
+    const {activityStore} = useStore();
+    const {deleteActivity, getActivitiesByDate: activities, loading} = activityStore
 
     function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string){
         setTarget(e.currentTarget.name);
@@ -30,11 +26,11 @@ export default function ActivityList({activities, selectActivity, deleteActivity
                                 <div>{activity.city}, {activity.venue}</div>
                             </Item.Description>
                             <Item.Extra>
-                                <Button onClick={() => selectActivity(activity.id)} floated='right' content='View' color='blue' />
+                                <Button onClick={() => activityStore.selectActivity(activity.id)} floated='right' content='View' color='blue' />
                                 <Button
                                     name={activity.id} 
                                     onClick={(e) => handleActivityDelete(e, activity.id)} 
-                                    loading={submitting && target === activity.id} 
+                                    loading={loading && target === activity.id} 
                                     floated='right' 
                                     content='Delete' 
                                     color='red' />
@@ -46,4 +42,4 @@ export default function ActivityList({activities, selectActivity, deleteActivity
             </Item.Group>
         </Segment>
     )
-}
+})
