32c0fd157a4423ad772fea8e8697a6a4bee0d322
Семен Попов <s.popov@avto.pro>
2023-07-16 13:50:55 +0300

    Add User About

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -5,9 +5,16 @@ namespace API.Controllers
 {
     public class ProfilesController : BaseApiController
     {
+        [HttpGet("{username}")]
         public async Task<IActionResult> GetProfile(string username)
         {
             return HandleResult(await Mediator.Send(new Details.Query { Username = username }));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> EditProfile(Edit.Command command)
+        {
+            return HandleResult(await Mediator.Send(command));
         }
     }
 }
